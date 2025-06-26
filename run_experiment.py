41a70e4ab32af7d71f3a9598ff07f0a68c1751bb
Emily <emily.tempus@embl.de>
2025-06-26 18:12:18 +0200

    fixed deletion of ckpt dir

--- run_experiment.py
+++ run_experiment.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     config = Config.from_yaml()
 
     # check_for_checkpoints(config.paths.checkpoint_dir)
-    shutil.rmtree(config.paths.checkpoint_dir)
+    if os.path.exists(config.paths.checkpoint_dir):
+        shutil.rmtree(config.paths.checkpoint_dir)
 
     trainer = latentModulatedTrainer(config.model, config.training, config.paths).to(
         config.model.device
